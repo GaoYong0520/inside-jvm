0fa596fddeb6dc5a828e8cac0ceecf83360d8601
Givemeablock <yangyuanchangdali@163.com>
2018-02-03 15:03:16 +0800

    java内存模型1

---
+++ jvm1/src/t12.java
@@ -0,0 +1,32 @@
+public class t12 {
+
+    public static volatile int race = 0;
+
+    public static void increase() {
+        race ++;
+    }
+
+    public static void main(String[] args) {
+
+        Thread[] t = new Thread[20];
+
+        for(int i=0;i<20;i++){
+            t[i]=new Thread(new Runnable(){
+                @Override
+                public void run(){
+                    for(int i=0;i<2000;i++){
+                        increase();
+                    }
+                }
+            });
+            t[i].start();
+        }
+//等待所有累加线程都结束
+        while(Thread.activeCount()>1) {
+            Thread.yield();
+            System.out.println(race);
+        }
+    }
+
+}
+
